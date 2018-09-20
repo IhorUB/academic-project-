c91149a9b0c2fdfcc65367a7ffda43750305a52c
Ihor <ihor.maryniukub@gmail.com>
2018-09-21 00:30:09 +0300

    change responsive slider and nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import '../../node_modules/jquery/dist/jquery.min.js';
 import $ from 'jquery'; /*add library*/
 
-
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import '../css/main.scss';
